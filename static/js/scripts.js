f51eba3e6e759aae0a4495abbe0a18fe1f43b3db
Kimjunghyunn <93319948+Kimjunghyunn@users.noreply.github.com>
2021-11-06 20:50:20 +0900

    css,js,index 수정/중간합침

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -79,6 +79,40 @@ function fadeIn(el, display) {
     })();
 };
 
+//배너 사진 렌덤 표출 함수
 let randomNumber = Math.floor(Math.random() * 4) + 1;
 document.querySelector('.masthead').classList.add('bg0' + randomNumber);
 
+//TMDB API DATA 불러와서 영화 포스터 카드 표출
+$(document).ready(function () {
+    showAPI();
+});
+
+function showAPI() {
+    $.ajax({
+        type: "GET",
+        url: "/API?",
+        data: {},
+        success: function (response) {
+            let api_read = response['api_loading']
+
+            //api_read.length만큼 반복하여 이름,포스터,내용,평점,투표수 등을 가져옴
+            for (let i = 0; i < api_read.length; i++) {
+                let title = api_read[i]['title']
+                let img_url = api_read[i]['poster_path']
+                //console.log(title, img_url)
+                let temp_html = `<div class="col-lg-6">
+                                    <a class="portfolio-item" href="#!">
+                                        <div class="caption">
+                                            <div class="caption-content">
+                                                <div class="h2">${title}</div>
+                                            </div>
+                                        </div>
+                                        <img class="img-fluid" src="https://image.tmdb.org/t/p/original${img_url}" alt="..." />
+                                    </a>
+                                </div>`
+                $('#row_gx_0').append(temp_html)
+            }
+        }
+    })
+}
